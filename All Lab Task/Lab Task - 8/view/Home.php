64ce5484da69a4ed87d8669e1f645c95c67ed456
TanjimulHasanSohan <102829620+TanjimulHasanSohan@users.noreply.github.com>
2023-04-30 21:47:17 +0600

    Add files via upload

---
+++ All Lab Task/Lab Task - 8/view/Home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <header>
+        <h2 class="logo"><b>6t4</b></h2>
+        <nav class="navigation">
+            <a href="#" class="active">Home</a>
+            <a href="signin.php">Sign in</a>
+            <a href="#">Contact</a>
+            <a href="#">About</a>
+
+        </nav>
+    </header>
+</body>
